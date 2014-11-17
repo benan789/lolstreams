9cef1e8d9041d5cff68579e1c7ea65076ccc4987
benan789 <benjamin_an@yahoo.com>
2014-11-17 00:33:39 -0800

    script require file change

--- script.rb
+++ script.rb
@@ -1,17 +1,4 @@
-require 'bundler'
-Bundler.require
-require 'sinatra'
-require "sinatra/cookies"
-require 'sinatra/assetpack'
-require 'redis'
-require 'oauth2'
-require 'json'
-require 'unirest'
-require 'dotenv'
-Dotenv.load
-Mongoid.load!("./mongoid.yml")
-require './models/streamer.rb'
-require './models/champion.rb'
+require './config.rb'
 require 'open-uri'
 
 redis = Redis.new
