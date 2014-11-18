ff18bc7bd71b75b96f5ea718d9ac67ddcb9dcc6b
benan789 <benjamin_an@yahoo.com>
2014-11-17 22:55:07 -0800

    added spectating status

--- script.rb
+++ script.rb
@@ -16,23 +16,27 @@ streams.each do |stream|
 					begin
 						if summoner != ""
 							puts summoner
-							begin
-								response = Unirest.get "https://community-league-of-legends.p.mashape.com/api/v1.0/#{region}/summoner/retrieveInProgressSpectatorGameInfo/#{URI::encode(summoner)}",
-								headers:{
-								  "X-Mashape-Key" => ENV['MASHAPE']
-								}
-								if response.body['error'] == "Game has not started"
-									stream['status'] = "Champion select."
-									stream['rank'] = "Champion Select"
-									stream['region'] = region
-									break
-								end
-								summoner_info = response.body['game']['playerChampionSelections']['array'].find {|x| x['summonerInternalName'] == summoner}
-								summoner_info2 = response.body['game']['teamOne']['array'].find {|x| x['summonerInternalName'] == summoner} || summoner_info2 = response.body['game']['teamTwo']['array'].find {|x| x['summonerInternalName'] == summoner}
-							rescue
-								puts "MASHAPE rate limit?"
+							
+							response = Unirest.get "https://community-league-of-legends.p.mashape.com/api/v1.0/#{region}/summoner/retrieveInProgressSpectatorGameInfo/#{URI::encode(summoner)}",
+							headers:{
+							  "X-Mashape-Key" => ENV['MASHAPE']
+							}
+							if response.body['error'] == "Game has not started"
+								stream['status'] = "Champion select."
+								stream['rank'] = "Champion Select"
+								stream['region'] = region
+								break
 							end
-
+							summoner_info = response.body['game']['playerChampionSelections']['array'].find {|x| x['summonerInternalName'] == summoner}
+							summoner_info2 = response.body['game']['teamOne']['array'].find {|x| x['summonerInternalName'] == summoner} || summoner_info2 = response.body['game']['teamTwo']['array'].find {|x| x['summonerInternalName'] == summoner}
+							
+							if summoner_info == nil
+								stream['status'] = "Spectating."
+								stream['rank'] = "Spectating"
+								stream['region'] = region
+								break
+							end
+								
 							begin
 								rank_response = Unirest.get "https://#{region.downcase}.api.pvp.net/api/lol/#{region.downcase}/v2.5/league/by-summoner/#{summoner_info2["summonerId"]}?api_key=#{ENV['LOL_SECRET']}"
 								rank = JSON.parse(rank_response.body.to_json)
@@ -54,7 +58,7 @@ streams.each do |stream|
 						stream['status'] = "Not in game."
 					end
 				end
-				break if stream['status'] == "Champion select." || stream['status'] == "In game."
+				break if stream['status'] == "Champion select." || stream['status'] == "In game." || stream['status'] == "Spectating."
 			end
 		rescue
 			stream['champion'] = "No info."
