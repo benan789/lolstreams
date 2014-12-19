41e3c56ac41048edd6197c5ae31e52fce1b2e002
Ben <benan1989@hotmail.com>
2014-12-18 23:42:34 -0800

    fav icon path

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ require 'json'
 require 'unirest'
 require 'dotenv'
 Dotenv.load
-Mongoid.load!("./mongoid.yml", :production)
+Mongoid.load!("./mongoid.yml")
 require './models/streamer.rb'
 require './models/champion.rb'
 
