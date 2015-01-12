89c5a6e68ced71d1c248ea92678b5a5cb6323d35
Ben <benan1989@hotmail.com>
2015-01-12 14:41:16 -0800

    test

--- script.rb
+++ script.rb
@@ -46,6 +46,8 @@ begin
 											  "X-Mashape-Key" => ENV['MASHAPE']
 											}
 
+										puts response.body
+
 										if response.body['data']['ECODE'] == "GAMENOTFOUND_PATH"
 											stream['champion'] = "Not in game."
 											stream['status'] = "Not in game."
